d1c959fa875571e6a8b03f6680f2ac5d990eac5c
Aivenest <aivenest@gmail.com>
2022-09-27 17:12:53 +0500

    Ex03 compleated

---
+++ Exercise03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PowN(int N)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        Console.Write(" " + Math.Pow(i, 3) + " ");
+        i++;
+    }
+}
+
+PowN(N);
